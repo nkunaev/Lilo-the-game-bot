90fd3326bcbebc32d02eda274dfba08e2b4ba440
nd.kunaev <nd.kunaev@severstal.com>
2024-10-01 23:38:00 +0300

    Create tg client, fetcher and processor

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/nkunaev/lilo-the-game-bot
+
+go 1.22.5
---
+++ server/server.go
@@ -0,0 +1,8 @@
+package server
+
+func main () {
+	// tgClient = telegram.New(token)
+	// fetcher = fecther.New()
+	// processor = processor.New()
+	// consumer.Start(fetcher, processor)
+}
